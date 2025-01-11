1026bc6a371a1e8855b6668c05642b4167a39788
postmeow <meow@pandora-2025.01>
2025-01-11 21:43:55 +0000

    - add dora daemon to init - add dora claim to init - add mkhostname: generated hostname based on words found in fortunes - fix zsh prompt - fix qemu serial - add disk to mktest - add dora claim disk - add dora auto mount

--- bootstrap/mkapkchroot/etc/zsh/zshrc.d/pandora.zsh
+++ bootstrap/mkapkchroot/etc/zsh/zshrc.d/pandora.zsh
@@ -2,10 +2,11 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 if [ "$TERM" = "fbterm" ]; then
         source /usr/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
 else
+	PROMPT="%n@%m:%~"
 	if [ "$(id -u)" -eq 0 ]; then
-	        PS1='\u@\H:\w# '
+		PROMPT=$PROMPT"# "
 	else
-	        PS1='\u@\H:\w$ '
+		PROMPT=$PROMPT"$ "
 	fi
 fi
 
